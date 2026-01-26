6481f196b3929b3ef7a76db5f22d5d8cbb46c285
naveenk2022 <naveen.kannan12@gmail.com>
2026-01-26 17:07:46 -0500

    Created OrderProduct as an association table to track Order-Product association.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This repository contains the code for an e-commerce relational data model. The models are built using SQLAlchemy, and the database migrations are generated and applied by Alembic. 
--- models.py
+++ models.py
@@ -13,54 +13,73 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
+
 class Customer(Base):
     __tablename__ = "customers"
-    
-    customer_id = Column(
-        Integer, Identity(always=True), primary_key=True
-    )
+
+    customer_id = Column(Integer, Identity(always=True), primary_key=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     email_address = Column(String, nullable=False)
-    
+
     orders = relationship(
         "Order",
         back_populates="customer",
         cascade="all, delete-orphan",
     )
 
+
+class OrderProduct(Base):
+    __tablename__ = "order_products"
+    order_id = Column(
+        Integer,
+        ForeignKey("orders.order_id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+
+    product_id = Column(
+        Integer,
+        ForeignKey("products.product_id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+
+    product_count = Column(Integer, server_default="1", nullable=False)
+    order = relationship("Order", back_populates="products")
+    product = relationship("Product", back_populates="orders")
+
+
 class Order(Base):
     __tablename__ = "orders"
-    
-    order_id = Column(
-        Integer, Identity(always=True), primary_key=True
-    )
+
+    order_id = Column(Integer, Identity(always=True), primary_key=True)
     customer_id = Column(
-        Integer,
-        ForeignKey("customers.customer_id", ondelete="CASCADE"),
-        nullable=False
+        Integer, ForeignKey("customers.customer_id", ondelete="CASCADE"), nullable=False
     )
     order_date = Column(
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
     )
-    
     customer = relationship(
-        "Customer", 
-        back_populates="orders"
+        "Customer",
+        back_populates="orders",
     )
+    products = relationship(
+        "OrderProduct",
+        back_populates="order",
+        cascade="all, delete-orphan",
+    )
+
 
 class Product(Base):
     __tablename__ = "products"
-    
-    product_id = Column(
-        Integer, Identity(always=True), primary_key=True
-    )
-    product_name = Column(
-        String, nullable=False
-    )
+
+    product_id = Column(Integer, Identity(always=True), primary_key=True)
+    product_name = Column(String, nullable=False)
     description = Column(Text)
     price = Column(Float, nullable=False)
-
-
+    orders = relationship(
+        "OrderProduct",
+        back_populates="product",
+        cascade="all, delete-orphan",
+    )
